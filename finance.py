1e0c4d6d11d46220df636d095c5ab6b7b1a43ec9
Deyvid Lins <deyvidg.lins@gmail.com>
2021-04-23 22:46:59 -0300

    Envio de e-mail do outlook e python

--- finance.py
+++ finance.py
@@ -1,20 +1,19 @@
 import os
 import time
-from main import enviar_email
-import subprocess
 
-os.startfile("ControleFinanceiro.xlsx")
+
+os.startfile(r"C:\Users\Deyvid\Desktop\ControleFinanceiro.xlsx")
 
 i = 0
 
 while i <= 1:
     time.sleep(10)
     try:
-        a = open("ControleFinanceiro.xlsx", "r+")
+        a = open(r"C:\Users\Deyvid\Desktop\ControleFinanceiro.xlsx", "r+")
         print("Arquivo está fechado")
         a.close()
+        os.system(r'python "C:\Users\Deyvid\Desktop\Controle Financeiro\main.py" ')
 
-        enviar_email()
         break
     except IOError:
         print("Arquivo está aberto")
--- main.py
+++ main.py
@@ -3,22 +3,68 @@ import psutil
 import os
 import subprocess
 import pandas as pd
+import time
+
+
+
 
 def enviar_email():
     leitura = pd.read_excel(r"C:\Users\Deyvid\Desktop\Controle Financeiro\ControleFinanceiro.xlsx")
     total = leitura['Unnamed: 8'][11]
-    print(total)
-
 
 
     def send_notification():
+        t = time.localtime()
+        if t[1]>0 and t[1]<9:
+            m = f'0{t[1]}'
+
+        else:
+            m = t[1]
+
+
+        if m == '01' :
+            mes = 'Janeiro'
+        elif m == '02':
+            mes = 'Fevereiro'
+        elif m == '03':
+            mes = 'Março'
+        elif m == '04':
+            mes = 'Abril'
+        elif m == '05':
+            mes = 'Maio'
+        if m == '06' :
+            mes = 'Junho'
+        elif t[1] == '07':
+            mes = 'Julho'
+        elif m == '08':
+            mes = 'Agosto'
+        if m == '09' :
+            mes = 'Setembro'
+        elif m == '10':
+            mes = 'Outubro'
+        elif m == '11':
+            mes = 'Novembro'
+        elif m == '12':
+            mes = 'Dezembro'
         outlook = win32.Dispatch('outlook.application')
         mail = outlook.CreateItem(0)
         mail.To = 'deyvinholins18@gmail.com; angelicasantos444@gmail.com'
-        mail.Subject = 'Relatório de Controle Financeiro Referente ao mês de Abril'
-        mail.body = f'''
-        Segue o total gasto referente ao mês de Abril
-        total:{total}'''
+        mail.Subject = 'Relatório de controle financeiro referente ao mês de Abril'
+        mail.HTMLbody = f'''
+        <p>Segue o total de gasto referente ao mês de <strong>{mes.capitalize()}</strong></p>
+            
+        <p>O seu total de gasto neste mês foi:<strong>{total}</strong></p>   
+       
+
+        <p>EM anexo, contém mais informações detalhadas</p><br><br><br><br><br><br><br><br><br>
+        
+        
+        <p> &#9888; &#9888; &#9888; <strong>ATENÇÂO!!!</strong> Tente não gastar muito para dormir mais tranquilo(a) e ficar bem financeiramente.</p>
+
+        <p>Arquivo modificado na data: <strong>{t[2]}/{m}/{t[0]} às {t[3]}:{t[4]}:{t[5]}</strong></p>'''
+
+        anexo = r"C:\Users\Deyvid\Desktop\Controle Financeiro\ControleFinanceiro.xlsx"
+        mail.Attachments.Add(anexo)
         mail.Send()
 
 
@@ -49,4 +95,4 @@ def enviar_email():
         open_outlook()
         send_notification()
 
-
+enviar_email()
